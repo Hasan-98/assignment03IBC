531dac8ac8cea89ea7bcb8be4a75cb3b8eade1b8
Hasan-98 <59702148+Hasan-98@users.noreply.github.com>
2020-11-02 23:03:25 +0500

    Update satoshi.go

--- satoshi.go
+++ satoshi.go
@@ -205,9 +205,7 @@ func VerifyChain(chainHead *Block) {
 	}
 }
 
-func SendChainandConnInfo1() {
 
-}
 
 func SendChainandConnInfo() {
 
@@ -272,34 +270,3 @@ var infoArray = make([]net.Conn, 5)
 var index = 0
 var chainHead *Block
 
-// func main() {
-// 	//it is better to check for arguments length and throw error
-// 	satoshiAddress := os.Args[1]
-// 	//should have used a setter and made the variable private
-// 	Quorum, _ = strconv.Atoi(os.Args[2])
-
-// 	//The function below launches and initializes the chain and the server
-// 	//It then starts a routine for each connection request received
-// 	//The listening address of each node and their conn info is then stored
-// 	//it is important not to sequentually do things in StartListening routine and
-// 	//rather use channel for communication between routines
-
-// 	go StartListening(satoshiAddress, "satoshi")
-
-// 	//this should block satoshi till the quorum is complete
-// 	//Hint: we can read from a channel to block a routine unless some other writes
-// 	WaitForQuorum()
-// 	log.Println("Quorum complete, satoshi unblocked!!")
-
-// 	//sends each peer, the address of another one to connect to in a ring based topology
-// 	//it also sends the current chain to each peer
-
-// 	//
-// 	SendChainandConnInfo()
-// 	//
-
-// 	//blocking the main routine, we have others working
-// 	//it is better to use this or (even reading from a channel never being written to)
-// 	//infinite for loop is not recommended
-// 	select {}
-// }
